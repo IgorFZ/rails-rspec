046a4028a242bc64dabb96b24a4ca7ff0b5cb3e8
IgorFZ <igorf.zamp@gmail.com>
2023-03-03 17:28:17 -0300

    Add Page New Word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,4 +3,23 @@ class WordsController < ActionController::Base
     def index
         @words = Word.all
     end
+
+    def new
+        @word = Word.new
+    end
+
+    def create
+        @word = Word.new(word_params)
+        if @word.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    private 
+
+    def word_params
+        params.require(:word).permit(:value, :language)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "words", to: "words#index"
+  resources :words, only: [:index, :new, :create]
+  post "words/new", to: "words#new"
+  root "words#index"
 end
