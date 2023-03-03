1619ee1bff379310fd6f0b4c51e50e68d0af91f8
IgorFZ <igorf.zamp@gmail.com>
2023-03-03 15:21:10 -0300

    Add validations and specs to words model

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Word, type: :model do
-  it { is_expected.to have_db_column(:value) }
-  it { is_expected.to have_db_column(:language) }
+  describe 'columns' do
+    it { is_expected.to have_db_column(:value) }
+    it { is_expected.to have_db_column(:language) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:value) }
+    it { is_expected.to validate_presence_of(:language) }
+  end
 end
